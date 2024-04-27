06728752f7606ca1317ccf4183827cd8951caa3e
João Alex <97920941+joaoalexarruda@users.noreply.github.com>
2024-04-27 18:53:43 -0300

    update motorcycles template and view

--- motorcycles/views.py
+++ motorcycles/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Motorcycle
+from django.db.models import Q
 
 # Create your views here.
 
@@ -9,10 +9,15 @@ class MotorcycleListView(ListView):
     model = Motorcycle
     template_name = 'motorcycles/motorcycle_list.html'
     context_object_name = 'motorcycle_list'
-    
+    paginate_by = 10
+
     def get_queryset(self):
         queryset = super().get_queryset().order_by('model')
         search = self.request.GET.get('search')
         if search:
-            queryset = queryset.filter(model__icontains=search)
+            queryset = queryset.filter(
+                Q(model__icontains=search) |
+                Q(manufacturer__name__icontains=search) |
+                Q(engine_size__icontains=search)
+            )
         return queryset
