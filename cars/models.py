3ab450fc22546b1df3df7da95e0c9640d5e169a5
yagodinarte <168201555+yagodinarte@users.noreply.github.com>
2024-04-28 16:44:22 -0300

    Create class Car database table

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Car(models.Model):
+    id = models.AutoField(primary_key=True)
+    model = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    factory_year = models.IntegerField(blank=True, null=True)
+    model_year = models.IntegerField(blank=True, null=True)
+    value = models.FloatField(blank=True, null=True)
+    body_type = models.CharField(max_length=200)
+    power = models.IntegerField(blank=True, null=True)
+    cylinder_number = models.IntegerField(blank=True, null=True)
+    fuel = models.CharField(max_length=200)
+    gearbox = models.CharField(max_length=200)
+    doors = models.IntegerField(blank=True, null=True)
+    quilometers = models.IntegerField(blank=True, null=True)
+
