41582af2162d3b0900adf854577cea9c141d369d
yagodinarte <168201555+yagodinarte@users.noreply.github.com>
2024-04-29 17:23:18 -0300

    create car_brand

--- cars/admin.py
+++ cars/admin.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from cars.models import Car
 
 
+
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value', 'body_type', "power", "cylinder_number", "fuel", "gearbox", "doors", "quilometers")
     search_fields = ('model','brand')
 
+
 admin.site.register(Car, CarAdmin)
+
