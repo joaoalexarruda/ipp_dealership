606ab44aa08a516d03319b3de8f4df7426d0abef
yagodinarte <168201555+yagodinarte@users.noreply.github.com>
2024-04-29 00:02:32 -0300

    ADD "brand" in search_fields

--- cars/admin.py
+++ cars/admin.py
@@ -4,6 +4,6 @@ from cars.models import Car
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value', 'body_type', "power", "cylinder_number", "fuel", "gearbox", "doors", "quilometers")
-    search_fields = ('model',)
+    search_fields = ('model','brand')
 
 admin.site.register(Car, CarAdmin)
