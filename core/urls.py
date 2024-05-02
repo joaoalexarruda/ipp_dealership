65f0fde4eb7d7752b323c8f6122e09472141ba65
João Alex <97920941+joaoalexarruda@users.noreply.github.com>
2024-05-02 20:05:45 -0300

    update urls

--- core/urls.py
+++ core/urls.py
@@ -5,13 +5,10 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Frontend URLs
     path('', include('pages.urls')),
     path('', include('motorcycles.urls')),
     path('', include('accounts.urls')),
     path('', include('cars.urls')),
-    # Backend URLs
-
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
