b1d7838b36290ef239921b0979bc72b35180c6e3
João Alex <97920941+joaoalexarruda@users.noreply.github.com>
2024-05-03 16:27:43 -0300

    update .gitignore

--- accounts/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('accounts/register/', views.register_view, name='register'),
-    path('accounts/login/', views.login_view, name='login'),
-    path('accounts/logout/', views.logout_view, name='logout'),
-]
