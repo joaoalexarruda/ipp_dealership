17fa451cc016e796c7146510d0271c22d7266f0d
yagodinarte <168201555+yagodinarte@users.noreply.github.com>
2024-05-03 10:36:29 -0300

    add refresh and verify token endpoints

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt import views
 
 
 urlpatterns = [
-    path('authentication/token/', TokenObtainPairView.as_view(),
+    path('token/', views.TokenObtainPairView.as_view(),
          name='token_obtain_pair'),
+    path('token/refresh/',
+         views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/verify/',
+         views.TokenVerifyView.as_view(), name='token_verify'),
 ]
